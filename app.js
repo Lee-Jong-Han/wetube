dd65ff552f12217b86d981b334949a76b6753d6e
LeeJongHan <opcr8522@naver.com>
2019-02-14 11:15:40 +0900

    1

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json({ extend: true }));
+app.use(bodyParser.urlencoded({ extend: true }));
+app.use(morgan("dev"));
+app.use(helmet());
+
+app.use("/", globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+export default app;
