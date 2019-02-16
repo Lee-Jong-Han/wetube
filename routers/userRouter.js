5fc31612aaffeb5cef92903deb9d7beba343c1d6
LeeJongHan <opcr8522@naver.com>
2019-02-16 12:51:07 +0900

    env 까지

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -10,9 +10,8 @@ import {
 
 const userRouter = express.Router();
 
-userRouter.get('/', users);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
-userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
